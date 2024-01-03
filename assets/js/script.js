e0710fdcb8807893b2e188b8faf9a555006ef510
elismountain <echospb@mail.ru>
2024-01-03 19:35:40 +0000

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let timerEl = document.getElementById("time");
+let startScreen = document.getElementById("start-screen");
+let startBtn = document.getElementById("start");
+let questions = document.getElementById("questions");
+let questionTitle = document.getElementById("question-title");
+let questionsChoices = document.getElementById("choices");
+let endScreen = document.getElementById("end-screen");
+let finalScore = document.getElementById("final-score");
+let initials = document.getElementById("initials");
+let submitBtn = document.getElementById("submit");
+let feedbackEl = document.getElementById("feedback");
+
+
+
+// Define the questions and the choices and the answers, put it in a variable in questions.js file
+
